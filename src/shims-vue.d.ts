ecdd92db39684e2da4046b6ef792e53cf07ca68e
周林 <634023699@qq.com>
2020-04-08 19:13:43 +0800

    配置ts使用第三方js库

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,20 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+import VueRouter from "vue-router";
+import { Store } from "vuex";
+import Vue from "vue";
+import { AxiosInstance } from "axios";
+
+declare module "*.vue" {
+  export default Vue;
+}
+declare module "vue/types/vue" {
+  interface Vue {
+    $axios: AxiosInstance;
+  }
+}
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    router?: VueRouter;
+    store?: Store<any>;
+  }
 }
